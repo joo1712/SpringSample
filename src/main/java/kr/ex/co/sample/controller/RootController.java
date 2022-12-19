e88fff08638d7a7fb58aa0756a33cb4214865128
joo1712 <45781461+joo1712@users.noreply.github.com>
2022-12-19 18:00:35 +0900

    스프링 시큐리티 적용 (1)

--- src/main/java/kr/ex/co/sample/controller/RootController.java
+++ src/main/java/kr/ex/co/sample/controller/RootController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,5 +25,12 @@ public class RootController {
 		logger.info("URI :" + req.getRequestURI());
 		logger.info("===============================================");
 		return new ModelAndView("index");
+	}
+	
+	@RequestMapping("/accessError")
+	public void accessError(Authentication auth, Model model) {
+		logger.info("access Denied : " + auth);
+		
+		model.addAttribute("msg", "Access Denied");
 	}
 }
